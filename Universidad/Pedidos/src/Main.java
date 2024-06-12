60092d6de21a4c648db8c9ae16fd8bb437e57c05
Santiago <sarivera.es@tecnologicoargos.edu.ec>
2024-06-12 14:34:31 -0500

    Main

---
+++ Universidad/Pedidos/src/Main.java
@@ -0,0 +1,27 @@
+package Universidad.Pedidos.src;
+
+// import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        /* Pedidos pedidos = new Pedidos(25, new Date(), 10.35, "Prueba");
+
+        System.out.println(pedidos.toString());
+
+        pedidos.solicitar();
+
+        pedidos.cancelarPedido();
+
+        pedidos.cancelarPedido(); */
+        
+        /*Cliente cliente = new Cliente(12, "Santiago", "Duran", "0995303033", "sarivera.es@tecnologicoargos.edu.ec", 19);
+
+        cliente.ListarNumeros();
+
+        cliente.actualizarDatos();
+
+        cliente.agregarPedido();*/
+
+    }    
+}
+
