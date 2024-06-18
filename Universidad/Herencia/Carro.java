70edc73c49949b30d8a16efbf97d4eb80b8d6068
Santiago <sarivera.es@tecnologicoargos.edu.ec>
2024-06-17 22:58:24 -0500

    Main

---
+++ Universidad/Herencia/Carro.java
@@ -0,0 +1,37 @@
+package Universidad.Herencia;
+
+public class Carro {
+    
+    //Atributos
+
+    private String duenio;
+
+    //Métodos
+
+    public void encenderAireAcondicionado(){
+        System.out.println("Encender el aire acondicionado");
+    }
+
+    //Getter y Setter
+
+    public String getDuenio() {
+        return duenio;
+    }
+
+    public void setDuenio(String duenio) {
+        this.duenio = duenio;
+    }
+
+    //Constructor
+
+    public Carro(String duenio) {
+        this.duenio = duenio;
+    }
+
+    public class Vehiculo extends Carro{
+
+        public Vehiculo(String duenio) {
+            super(duenio);
+        }
+    }
+}
---
+++ Universidad/Herencia/Main.java
@@ -0,0 +1,10 @@
+package Universidad.Herencia;
+
+import java.util.Date;
+
+public class Main {
+
+    Vehiculo vehiculo = new Vehiculo("Mustang", 215, new Date());
+
+    Carro carro = new Carro("Santiago");
+}
