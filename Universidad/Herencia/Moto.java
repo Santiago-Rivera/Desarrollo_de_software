736585aee83567d99a51a44dae55fb9692c0e721
Santiago <sarivera.es@tecnologicoargos.edu.ec>
2024-06-26 20:58:36 -0500

    Herencia modificado 2

--- Universidad/Herencia/Moto.java
+++ Universidad/Herencia/Moto.java
@@ -1,30 +1,31 @@
 package Universidad.Herencia;
 
-public class Moto {
+public class Moto extends Vehiculo {
 
     //Atributos
 
-    private int tieneSidecar;
+    private boolean tieneSidecar;
 
     //Métodos
 
     public void hacerCaballito(){
         System.out.println("El dueño de la moto hace caballito");
     }
-
+    
     // Getter y Setter
 
-    public int getTieneSidecar() {
+    public boolean getTieneSidecar() {
         return tieneSidecar;
     }
 
-    public void setTieneSidecar(int tieneSidecar) {
+    public void setTieneSidecar(boolean tieneSidecar) {
         this.tieneSidecar = tieneSidecar;
     }
 
     //Costructor
 
-    public Moto(int tieneSidecar) {
+    public Moto(boolean tieneSidecar, String marca, int modelo, Integer anio) {
+        super(marca, modelo, anio);
         this.tieneSidecar = tieneSidecar;
     }
 }
