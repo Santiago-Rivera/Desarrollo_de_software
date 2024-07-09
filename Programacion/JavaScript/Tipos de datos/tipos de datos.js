aeac8f8114fc6c89491f5e20a6cb3160b97dae9c
Santiago <sarivera.es@tecnologicoargos.edu.ec>
2024-07-09 15:01:59 -0500

    Tipos de datos

---
+++ Programacion/JavaScript/Tipos de datos/tipos de datos.js
@@ -0,0 +1,18 @@
+//Tipos de datos
+
+//Numerico
+
+var edad = 20
+
+var año = 23.90
+
+//Cadenas de texto
+
+var nombre = "Santiago"
+
+var apellido = "Rivera"
+
+document.write(nombre + " " + apellido + " " + edad)
+
+//Arreglo
+
