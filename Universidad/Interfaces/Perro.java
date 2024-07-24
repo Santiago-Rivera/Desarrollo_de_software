58ac5cf66ebb1f40ed11deba4ad651dac6af9b7c
Santiago <sarivera.es@tecnologicoargos.edu.ec>
2024-07-24 10:28:27 -0500

    Interfaces

---
+++ Universidad/Interfaces/Perro.java
@@ -0,0 +1,20 @@
+package Universidad.Interfaces;
+
+public class Perro implements Animal {
+
+    @Override
+    public void caminar() {
+        System.out.println("El perro esta caminando");
+    }
+
+    @Override
+    public void hacerRuido() {
+        System.out.println("Grrrrrr");
+    }
+
+    @Override
+    public String tipoAnimal() {
+        return "Labrador";
+    }
+
+}
