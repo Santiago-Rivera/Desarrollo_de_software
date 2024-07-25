332073e8c23c77bb8b0cc74f23f5ea52ca1bc951
Santiago <sarivera.es@tecnologicoargos.edu.ec>
2024-07-24 21:28:22 -0500

    Interfaces

--- Universidad/Interfaces/Gato.java
+++ Universidad/Interfaces/Gato.java
@@ -1,5 +1,19 @@
 package Universidad.Interfaces;
 
-public class Gato {
-    
+public class Gato implements Animal {
+
+    @Override
+    public void caminar() {
+        System.out.println("El gato esta caminando");
+    }
+
+    @Override
+    public void hacerRuido() {
+        System.out.println("Miau");
+    }
+
+    @Override
+    public String tipoAnimal() {
+        return "Es de tipo: Persa";
+    }
 }
