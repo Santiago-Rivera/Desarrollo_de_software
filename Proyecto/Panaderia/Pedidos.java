f5f08257008d83ad982d094f98ef69173655a1d8
Santiago <sarivera.es@tecnologicoargos.edu.ec>
2024-07-30 10:17:08 -0500

    Proyecto P4

--- Proyecto/Panaderia/Pedidos.java
+++ Proyecto/Panaderia/Pedidos.java
@@ -115,12 +115,7 @@ public class Pedidos extends Panaderia {
     }
 
     //Constructor
-  
-    public Pedidos(int idPedidos, Date fecha, double total, String estado, String productovendido) {
-        this.idPedidos = idPedidos;
-        this.fecha = fecha;
-        this.total = total;
-        this.estado = estado;
+
     public Pedidos(int idPedidos, Date fecha, double total, String registro, String productovendido, int idPanaderia, String nombre, String direccion, String paginaweb, String telefono, String sucursales, String redessociales) {
         super(idPanaderia, nombre, direccion, paginaweb, telefono, sucursales, redessociales);
         this.idPedidos = idPedidos;
