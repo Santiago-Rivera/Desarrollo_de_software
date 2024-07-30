bbe20ce7ec41028155d41300a331c8e3cca26485
Santiago <sarivera.es@tecnologicoargos.edu.ec>
2024-07-29 20:42:23 -0500

    Colecciones

--- Universidad/Colecciones/Main.java
+++ Universidad/Colecciones/Main.java
@@ -1,8 +1,25 @@
 package Universidad.Colecciones;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+    
+        List<Persona> alumnos = new ArrayList<>();
+
+        Persona alumno_1 = new Persona("Juan", 9);
+
+        Persona alumno_2 = new Persona("Jennifer", 12);
+
+        alumnos.add(alumno_1);
+
+        alumnos.add(alumno_2);
+
+        for (Persona persona : alumnos ) {
+            System.out.println(persona.getNombre() + " " + persona.getEdad());
+        }
     }
 }
