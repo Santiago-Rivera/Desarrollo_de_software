169452788de5148d6be77bfacad7a94fd791bb8a
Santiago <sarivera.es@tecnologicoargos.edu.ec>
2024-08-07 15:17:15 -0500

    SQL

---
+++ Programacion/SQL/Writing/02-Update.sql
@@ -0,0 +1,11 @@
+SELECT * FROM users;
+
+UPDATE users SET user_date = "46" WHERE user_id = 2;
+
+UPDATE users SET int_date = "2023-12-23" WHERE user_id = 5;
+
+UPDATE users SET user_lastname = "hernandez" WHERE user_id = 2;
+
+UPDATE users SET email = "franciscotobar@hotmail.com" WHERE user_id = 6;
+
+UPDATE users SET user_date = "20", int_date = "2024-01-01" WHERE user_id = 4;
---
+++ Programacion/SQL/Writing/03-Delete.sql
@@ -0,0 +1,3 @@
+SELECT * FROM users;
+
+DELETE FROM users WHERE user_id = 5;
