8b3216e9b5a84d4f16be06f1d2b983fb18b6de33
Santiago <sarivera.es@tecnologicoargos.edu.ec>
2024-10-08 19:37:06 -0500

    Ejercicios en Python

---
+++ Universidad/Ejercicios en Python/Ejercicios.py
@@ -0,0 +1,17 @@
+# Definimos las operaciones
+operacion_a = (3 * (3 + 4) * (5 - 2))
+operacion_b = ((3 ** (2 + 3)) - 1) ** (1.0 / 2.0)
+operacion_c = 5.0 / 2.0 * 3 + 4 ** 3.0 * 2 / (5.0 + 2.0) - 2
+operacion_d = 10 * (7 + 7) % (9 + 2) / 10
+operacion_e = 3 % 2 - (2 + 2) / 1 * (3 + 1) + 3
+operacion_f = (5 + 8 * 2 - 3.0 / 5.0) / (4 * 1 - 2.0 / 3.0 + 8 / 2)
+operacion_g = (4 + 8) / (4 / (1 + 1)) * (3 + 2 + 1) / (5 ** 2 + 4)
+
+# Imprimimos los resultados
+print("Resultado de la operación a:", operacion_a)
+print("Resultado de la operación b:", operacion_b)
+print("Resultado de la operación c:", operacion_c)
+print("Resultado de la operación d:", operacion_d)
+print("Resultado de la operación e:", operacion_e)
+print("Resultado de la operación f:", operacion_f)
+print("Resultado de la operación g:", operacion_g)
